d26fee44b3060333db8d055370555034f8be562e
Saeed Hajizade <saeed.hjz90@gmail.com>
2023-11-04 05:38:21 +0330

    Initial Commit

---
+++ MediaPreview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediaPreview
+//
+//  Created by Saeed on 8/13/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediaPreview/MediaPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPreviewApp.swift
+//  MediaPreview
+//
+//  Created by Saeed on 8/13/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct MediaPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
