09f9032d9a1f1734e9873bca700e1ef5f799e2f9
Saeed Hajizade <saeed.hjz90@gmail.com>
2023-11-04 16:14:04 +0330

    Add ImageList view model

---
+++ MediaPreview/ImageListVM.swift
@@ -0,0 +1,45 @@
+//
+//  ImageListVM.swift
+//  MediaPreview
+//
+//  Created by Saeed on 8/13/1402 AP.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+class ImageListViewModel: ObservableObject {
+    
+    @Published var images: [String: UIImage] = [:]
+    @Published var mediaList: [Media] = []
+    
+    private var cancellable: AnyCancellable?
+    private var cancellableSet: Set<AnyCancellable> = []
+    
+    let networkService: NetworkService
+    
+    init(networkService: NetworkService) {
+        self.networkService = networkService
+        fetchImageURLs()
+    }
+    
+    
+    func fetchImageURLs() {
+        cancellable = networkService.fetchMediaList()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { _ in }, receiveValue: { mediaList in
+                self.mediaList = mediaList
+            })
+    }
+    
+    
+    func downloadImages(at link: String) {
+        networkService.downloadImage(from: link)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { _ in }, receiveValue: { image in
+                self.images[link] = image
+            }).store(in: &cancellableSet)
+    }
+
+}
