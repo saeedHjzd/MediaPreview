d557db9de9442db76de5dc96d575678c98c01ca0
Saeed Hajizade <saeed.hjz90@gmail.com>
2023-11-04 05:51:32 +0330

    Add Media data model

---
+++ MediaPreview/MediaModel.swift
@@ -0,0 +1,16 @@
+//
+//  MediaModel.swift
+//  MediaPreview
+//
+//  Created by Saeed on 8/13/1402 AP.
+//
+
+import Foundation
+
+struct Media: Identifiable, Decodable, Hashable {
+    let id = UUID()
+    let title: String
+    let previewLink: String
+    let mediaLink: String
+    let type: String
+}
