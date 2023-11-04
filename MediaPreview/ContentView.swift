e05b5e48098aecc7c11fb85f130032e4412ffd70
Saeed Hajizade <saeed.hjz90@gmail.com>
2023-11-04 05:47:31 +0330

    Initial commit

--- MediaPreview/ContentView.swift
+++ MediaPreview/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Image(systemName: "photo.artframe")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .cornerRadius(10)
     }
 }
 
